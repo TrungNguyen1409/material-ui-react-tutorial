c155355b54d35d724014dd18f486567c32692916
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-08-11 16:14:45 +0200

    complete

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import SaveIcon from '@mui/icons-material/Save'
 import DeleteIcon from '@mui/icons-material/Delete'
 import CheckboxExample from './components/CheckboxExample';
 import TextField from './components/TextFieldExample';
-
 import { makeStyles } from '@mui/styles';
-import { styled } from '@mui/system';
-
+import { styled, Container } from '@mui/system';
+import "@fontsource/roboto";
+import { Grid, Paper, Typography } from '@mui/material';
 
 const MyButton = styled(Button)({
   background: 'linear-gradient(45deg, #333, #999)',
-    border: 0,
-    borderRadius: 15,
-    color: 'white',
-    padding: '0 30px'
+  border: 0,
+  borderRadius: 15,
+  color: 'white',
+  padding: '0 30px'
 });
 function StyledButton() {
   return <MyButton>Styled Button</MyButton>;
@@ -32,9 +32,9 @@ const useStyles = makeStyles({
   }
 })
 
-function ButtonStyled(){
+function ButtonStyled() {
   const classes = useStyles()
-  return <Button classname ={classes.root}>Test Styled Button</Button>
+  return <Button classname={classes.root}>Test Styled Button</Button>
 
 }
 
@@ -45,43 +45,67 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <StyledButton/>
-        <ButtonStyled/>
-        <TextField></TextField>
-        <CheckboxExample />
-        <ButtonGroup>
-          <Button
-            startIcon={<SaveIcon sx={{ fontSize: 100 }} />}
-            size="large"
-            style={{
-              fontSize: 20
-            }}
-            onClick={sayHello}
-            variant="contained"
-            color='primary'>
-            Save
-          </Button>
+    <Container maxWidth="sm">
+      <div className="App">
+        <header className="App-header">
+          <Typography variant="h2">
+            Welcome to MUI
+          </Typography>
+          <Typography variant="body1">
+            Learn how to use Material UI
+          </Typography>
+          <StyledButton />
+          <ButtonStyled />
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={12} sm={6}>
+              <Paper style={{ height: 75, width: '100%', }} />
+            </Grid>
+            <Grid item>
+              <Paper style={{ height: 75, width: 50, }} />
 
-          <Button
+            </Grid>
+            <Grid item>
+              <Paper style={{ height: 75, width: 50, }} />
 
-            startIcon={<DeleteIcon sx={{ fontSize: 100 }} />}
-            size="large"
-            style={{
-              fontSize: 20
-            }}
-            variant="contained"
-            color='secondary'>
-            Discard
-          </Button>
-        </ButtonGroup>
+            </Grid>
+          </Grid>
 
 
-        <img src={logo} className="App-logo" alt="logo" />
+          <TextField></TextField>
+          <CheckboxExample />
+          <ButtonGroup>
+            <Button
+              startIcon={<SaveIcon sx={{ fontSize: 100 }} />}
+              size="large"
+              style={{
+                fontSize: 20
+              }}
+              onClick={sayHello}
+              variant="contained"
+              color='primary'>
+              Save
+            </Button>
 
-      </header>
-    </div>
+            <Button
+
+              startIcon={<DeleteIcon sx={{ fontSize: 100 }} />}
+              size="large"
+              style={{
+                fontSize: 20
+              }}
+              variant="contained"
+              color='secondary'>
+              Discard
+            </Button>
+          </ButtonGroup>
+
+
+          <img src={logo} className="App-logo" alt="logo" />
+
+        </header>
+      </div>
+    </Container>
+
   );
 }
 
